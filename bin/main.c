630c0bc8608423f9ebebe4c504675e6275d855fc
xirzo <easttem@gmail.com>
2025-03-18 19:37:04 +0300

    feat: error handling

--- bin/main.c
+++ bin/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "routes.h"
@@ -6,7 +7,7 @@
 #define PORT "5000"
 #define MAX_PENDING_CONNECTIONS 10
 #define MAX_NUMBER_OF_ROUTES 10
-#define MAX_REQUEST_SIZE 1024 * 1024
+#define MAX_REQUEST_SIZE 16 * 1024 * 1024
 
 int main(int argc, char *argv[]) {
     Routes *r = createRoutes(MAX_NUMBER_OF_ROUTES);
@@ -15,7 +16,11 @@ int main(int argc, char *argv[]) {
 
     Server *s = createServer(PORT, MAX_PENDING_CONNECTIONS, MAX_REQUEST_SIZE, r);
 
-    startServer(s);
+    if (startServer(s) != 0) {
+        fprintf(stderr, "error: Failed to start server\n");
+        freeServer(s);
+        return EXIT_FAILURE;
+    }
 
     while (isServerRunning(s)) {
         acceptClientConnection(s);
